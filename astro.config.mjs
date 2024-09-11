b59dd0f09ce5080830f5892d8746f08422cbc7db
Maurico <abel772003@gmail.com>
2024-09-11 00:33:25 -0300

    subo configuracines del hostinger

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://grey-owl-456011.hostingersite.com/',
+});
